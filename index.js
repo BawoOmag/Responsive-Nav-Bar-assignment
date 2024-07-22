d85e991b141d392772a39ea09566786bf96dc6f1
Bawo <omagbemibawo@gmail.com>
2024-07-22 17:07:51 +0100

    Responsive Nav

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let menu = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".links")
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-x")
+    navbar.classList.toggle("open")
+}
